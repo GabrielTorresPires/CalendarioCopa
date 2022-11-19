12c5487e873b5ad9c477d623bc732231017058db
Gabriel Torres <gabrieltlpires@gmail.com>
2022-11-18 22:41:12 -0300

    Adiciona datas de 4 países

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿Console.WriteLine("--- Calendário Copa do Mundo 2022 ---");
+Console.WriteLine("--- Descubra as datas dos jogos das seleções ---");
+
+Console.WriteLine("1 - Alemanha");
+Console.WriteLine("2 - Arábia Saudita");
+Console.WriteLine("3 - Argentina");
+Console.WriteLine("4 - Austrália");
+Console.WriteLine("5 - Bélgica");
+Console.WriteLine("6 - Brasil");
+Console.WriteLine("7 - Camarões");
+Console.WriteLine("8 - Canadá");
+Console.WriteLine("9 - Catar");
+Console.WriteLine("10 - Coreia do Sul");
+Console.WriteLine("11 - Costa Rica");
+Console.WriteLine("12 - Croácia");
+Console.WriteLine("13 - Dinamarca");
+Console.WriteLine("14 - Equador");
+Console.WriteLine("15 - Espanha");
+Console.WriteLine("16 - Estados Unidos");
+Console.WriteLine("17 - França");
+Console.WriteLine("18 - Gana");
+Console.WriteLine("19 - Holanda");
+Console.WriteLine("20 - Inglaterra");
+Console.WriteLine("21 - Irã");
+Console.WriteLine("22 - Japão");
+Console.WriteLine("23 - Marrocos");
+Console.WriteLine("24 - México");
+Console.WriteLine("25 - País de Gales");
+Console.WriteLine("26 - Polônia");
+Console.WriteLine("27 - Portugal");
+Console.WriteLine("28 - Senegal");
+Console.WriteLine("29 - Sérvia");
+Console.WriteLine("30 - Suíça");
+Console.WriteLine("31 - Tunísia");
+Console.WriteLine("32 - Uruguai");
+
+Console.Write("Digite o número conrrespondente ao país:");
+int Numerodigitado = Convert.ToInt16 (Console.ReadLine()!);
+
+if (Numerodigitado == 1)
+{
+    Console.WriteLine(" Alemanha x Japão - Quarta - 23/11/2022 -10:00 hrs");
+    Console.WriteLine(" Espanha x Alemanha - Domingo - 27/11/2022 - 16:00 hrs");
+    Console.WriteLine(" Costa Rica x Alemanha - Quinta - 01/12/2022 - 16:00 hrs");
+   
+}
+
+if (Numerodigitado == 2)
+{
+    Console.WriteLine(" Argentina x Arábia Saudita - Terça - 22/11/2022 - 7:00 hrs");
+    Console.WriteLine(" Polônia x Arábia Saudita - Sábado - 26/11/2022 - 10:00 hrs");
+    Console.WriteLine(" Arábia Saudita x México - Quarta - 30/11/2022 - 16:00 hrs");
+   
+}
+
+if (Numerodigitado == 3)
+{
+    Console.WriteLine(" Argentina x Arábia Saudita - Terça - 22/11/2022 - 7:00 hrs");
+    Console.WriteLine(" Argentina x México - Sábado - 26/11/2022 - 16:00 hrs");
+    Console.WriteLine(" Polônia x Argentina - Quarta - 30/11/2022 - 16:00 hrs");
+   
+}
+
+
+if (Numerodigitado == 9)
+{
+    Console.WriteLine(" Catar x Ecuador - Domingo - 20/11/2022 -13:00 hrs");
+    Console.WriteLine(" Catar x Senegal - Sexta - 25/11/2022 - 10:00 hrs");
+    Console.WriteLine(" Holanda x Catar - Terça - 29/11/2022 - 12:00 hrs");
+   
+}
+
+
